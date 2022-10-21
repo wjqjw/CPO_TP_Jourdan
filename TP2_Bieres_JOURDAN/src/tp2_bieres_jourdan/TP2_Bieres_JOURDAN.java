5a19f63d96aa057d43b359c6c8ad0645bc9e4cda
Dell <Dell@DESKTOP-VJLVIIN.education.lakanal.epf.fr>
2022-10-21 15:56:36 +0200

    Commit 1 TP2

---
+++ TP2_Bieres_JOURDAN/src/tp2_bieres_jourdan/TP2_Bieres_JOURDAN.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_jourdan;
+
+/**
+ *
+ * @author Dell
+ */
+public class TP2_Bieres_JOURDAN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson"); 
+        uneBiere.lireEtiquette();
+        
+        BouteilleBiere uneBiere2 = new BouteilleBiere("shouffe", 9.9 ,"tuson") ;
+        uneBiere2.lireEtiquette();
+        
+        BouteilleBiere uneBiere3 = new BouteilleBiere("P-dieux", 8.9 ,"lillas") ;
+        uneBiere3.lireEtiquette();
+        
+    }
+        
+
+        
+    }
+    
+}
