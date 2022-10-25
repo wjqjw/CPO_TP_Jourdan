b6164ca636eb155242a65f110c49090d7e628e1f
Dell <Dell@DESKTOP-VJLVIIN.education.lakanal.epf.fr>
2022-10-25 11:23:49 +0200

    TP2 ex2 fin

--- TP2_convertisseurObjet_Jourdan/src/main/java/com/mycompany/tp2_convertisseurobjet_jourdan/TP2_convertisseurObjet_Jourdan.java
+++ TP2_convertisseurObjet_Jourdan/src/main/java/com/mycompany/tp2_convertisseurobjet_jourdan/TP2_convertisseurObjet_Jourdan.java
@@ -5,6 +5,8 @@
 
 package com.mycompany.tp2_convertisseurobjet_jourdan;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Dell
@@ -12,9 +14,81 @@ package com.mycompany.tp2_convertisseurobjet_jourdan;
 public class TP2_convertisseurObjet_Jourdan {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        convertisseur temp = new convertisseur ();
+        
+        double result = temp.CelciusVersKelvin(40);
+        System.out.println(result);
+        
+        double result1 = temp.KelvinVersCelcius(160);
+        System.out.println(result1);
+        
+        double result3 = temp.KelvinVersFarenheit(120);
+        System.out.println(result3);
+        
+        double result4 = temp.FarenheitVersKelvin(60);
+        System.out.println(result4);
+        
+        double result5 = temp.CelciusVersFarenheit(32);
+        System.out.println(result5);
+        
+        double result6 = temp.FarenheitVersCelcius(56);
+        System.out.println(result6);
+    
+    Scanner sc;
+    sc = new Scanner (System.in);
+    
+    System.out.println("Choississez votre entre : 1:Celcius / 2:Kelvin / 3:Farenheit");
+    int a = sc.nextInt();
+    System.out.println("Choississez votre sortie : 1:Celcius / 2:Kelvin / 3:Farenheit");
+    int b = sc.nextInt();
+    System.out.println("Quelle est la temperature a convertir");
+    int c = sc.nextInt();
+    
+    
+    if (a==1 && b==2){
+    double result12 = temp.CelciusVersKelvin(c);
+    System.out.println(result12);
+        
     }
     
+    if (a==1 && b==3){
+    double result13 = temp.CelciusVersFarenheit(c);
+    System.out.println(result13);
+        
+    }
+    
+        if (a==2 && b==1){
+    double result21 = temp.KelvinVersCelcius(c);
+    System.out.println(result21);
+        
+    }
+    if (a==2 && b==3){
+    double result23 = temp.KelvinVersFarenheit(c);
+    System.out.println(result23);
+        
+    }
+    
+    if (a==3 && b==1){
+    double result31 = temp.FarenheitVersCelcius(c);
+    System.out.println(result31);
+        
+    }
+    
+    if (a==3 && b==2){
+    double result32 = temp.FarenheitVersKelvin(c);
+    System.out.println(result32);
+        
+    }
+        
+       
+        
+        
+    }
+
+    
 }
+    
+    
+
 
 
--- TP2_convertisseurObjet_Jourdan/src/main/java/com/mycompany/tp2_convertisseurobjet_jourdan/convertisseur.java
+++ TP2_convertisseurObjet_Jourdan/src/main/java/com/mycompany/tp2_convertisseurobjet_jourdan/convertisseur.java
@@ -14,46 +14,54 @@ public class convertisseur {
  public convertisseur(){    
 nbConversions = 0 ;
 }
-public String toString () {
- return "nb de conversions"+ nbConversions;
-}
+
 
 public double CelciusVersKelvin(double Tc){
     double Tk;
     Tk= Tc + 273.15;
+    nbConversions+=1;
     return Tk;
     
 }
 public double KelvinVersCelcius(double Tk){
     double Tc;
     Tc= Tk-273.15;
+    nbConversions+=1;
     return Tc;
        
 }
 public double FarenheitVersCelcius(double Tf){
     double Tc;
     Tc= (Tf-32.0)/(5.0/9.0);
+    nbConversions+=1;
     return Tc;
        
 }
 public double CelciusVersFarenheit(double Tc){
     double Tf;
     Tf= (Tc+32.0)*(5.0/9.0);
+    nbConversions+=1;
     return Tf;
        
 }
 public double FarenheitVersKelvin(double Tf){
     double Tk;
     Tk=(Tf-32)*5.0/9.0+273.15 ;
+    nbConversions+=1;
     return Tk;
        
 }
 public double KelvinVersFarenheit(double Tk){
     double Tf;
     Tf= (Tk-273.15)*(9.0/5.0)+32;
+    nbConversions+=1;
     return Tf;
        
 }
+@Override
+public String toString () {
+ return "nb de conversions"+ nbConversions;
+
 
     
 }
